0a36f5ede8bcc387658a15046ef3e07f21d5a7d8
Hussam Alafandi <hosam.alafandi@gmail.com>
2025-03-28 08:52:25 +0100

    Use hash of the full config file

--- 02/conditional_gan/checkpoint.py
+++ 02/conditional_gan/checkpoint.py
@@ -6,15 +6,21 @@ import os
 import torch
 import wandb
 import yaml
+import json
 
 logger = logging.getLogger(__name__)
 
+def get_dict_hash(dictionary):
+    # Convert the dictionary to a JSON string (sorted keys ensure consistent hash)
+    dict_str = json.dumps(dictionary, sort_keys=True)
+    # Generate a SHA-256 hash of the string
+    return hashlib.sha256(dict_str.encode()).hexdigest()[:8]
 
 def generate_checkpoint_folder(config):
     # Create a string from key hyperparameters
-    key_params = f"{config['lr']}_{config['batch_size']}_{config['latent_dim']}_{config['ngf']}_{config['ndf']}"
+    # key_params = f"{config['lr']}_{config['batch_size']}_{config['latent_dim']}_{config['ngf']}_{config['ndf']}"
 
-    config_hash = hashlib.sha256(key_params.encode()).hexdigest()[:8]
+    config_hash = get_dict_hash(config)
     base_path = config.get("checkpoint_dir", "checkpoints")
     os.makedirs(base_path, exist_ok=True)
     folder_name = f"dcgan_lr{config['lr']}_bs{config['batch_size']}_latent{config['latent_dim']}_ngf{config['ngf']}_ndf{config['ndf']}_{config_hash}"
